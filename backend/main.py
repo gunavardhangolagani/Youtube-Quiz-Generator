3b3fff6e9fbe59869a870ad1d5be4a35b76115d0
gunavardhangolagani <gunavardhangolagani@gmail.com>
2025-09-25 20:00:06 +0530

    allowed render in CORS

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://youtube-quiz-generator-sage.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
