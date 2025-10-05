c7b7ba42ca72a2e16179daf6e86488d2e3184964
gunavardhangolagani <gunavardhangolagani@gmail.com>
2025-10-05 21:40:55 +0530

    port added

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://youtube-quiz-generator-sage.vercel.app"],
+    allow_origins=["https://6dd3bmv5-5173.inc1.devtunnels.ms", "http://127.0.0.1:5173","https://youtube-quiz-generator-sage.vercel.app","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
